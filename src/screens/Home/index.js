46187ef3c8fbf68fbbed9f6e551545aa6a61163b
Santi López Bayo <santitipro@hotmail.com>
2021-03-02 10:47:32 +0100

    navigation

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import {Button} from '@taxdown/piccolo/native';
 
 function Home({navigation}) {
   return (
     <View>
       <Text>Hello world</Text>
+      <Button title="epa" onClick={() => {}} />
       <Text onPress={() => navigation.navigate('Auth')}>LOGOUT</Text>
     </View>
   );
